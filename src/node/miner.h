ismaelsadeeq/bitcoin

[BlockAssembler]: return selected packages feerate and vsize
- The commit also test that block assembler now returns selected package
  vsize's and fee rates.

Co-authored-by: Pieter Wuille <bitcoin-dev@wuille.net>

Co-authored-by: ismaelsadeeq <ask4ismailsadiq@gmail.com>

src/node/miner.h
@@ -6,6 +6,7 @@
 #ifndef BITCOIN_NODE_MINER_H
 #define BITCOIN_NODE_MINER_H
 
+#include <policy/feerate.h>
 #include <policy/policy.h>
 #include <primitives/block.h>
 #include <txmempool.h>
@@ -13,6 +14,7 @@
 #include <memory>
 #include <optional>
 #include <stdint.h>
+#include <tuple>
 
 #include <boost/multi_index/identity.hpp>
 #include <boost/multi_index/indexed_by.hpp>
@@ -38,6 +40,7 @@ struct CBlockTemplate
     std::vector<CAmount> vTxFees;
     std::vector<int64_t> vTxSigOpsCost;
     std::vector<unsigned char> vchCoinbaseCommitment;
+    std::vector<std::tuple<CFeeRate, uint64_t>> vFeeratePerSize;
 };
 
 // Container for tracking updates to ancestor feerate as we include (parent)
@@ -143,6 +146,7 @@ private:
     uint64_t nBlockSigOpsCost;
     CAmount nFees;
     std::unordered_set<Txid, SaltedTxidHasher> inBlock;
+    std::vector<std::tuple<CFeeRate, uint64_t>> feerate_per_size;
 
     // Chain context for the block
     int nHeight;

magnyon/bitcoind

src/node/miner.h
@@ -7,6 +7,7 @@
 #define BITCOIN_NODE_MINER_H
 
 #include <node/types.h>
+#include <policy/feerate.h>
 #include <policy/policy.h>
 #include <primitives/block.h>
 #include <txmempool.h>
@@ -14,6 +15,7 @@
 #include <memory>
 #include <optional>
 #include <stdint.h>
+#include <tuple>
 
 #include <boost/multi_index/identity.hpp>
 #include <boost/multi_index/indexed_by.hpp>
@@ -39,6 +41,7 @@ struct CBlockTemplate
     std::vector<CAmount> vTxFees;
     std::vector<int64_t> vTxSigOpsCost;
     std::vector<unsigned char> vchCoinbaseCommitment;
+    std::vector<std::tuple<CFeeRate, uint32_t>> vFeeratePerSize;
 };
 
 // Container for tracking updates to ancestor feerate as we include (parent)
@@ -148,6 +151,7 @@ private:
     uint64_t nBlockSigOpsCost;
     CAmount nFees;
     std::unordered_set<Txid, SaltedTxidHasher> inBlock;
+    std::vector<std::tuple<CFeeRate, uint32_t>> feerate_per_size;
 
     // Chain context for the block
     int nHeight;